f70cd20f6481dfa10720c43b740a70c402a37136
Riccardo Binetti <rbino@gmx.com>
2023-03-03 01:45:23 +0100

    Initial commit
    Generated with:
    mix new tigerbeetlex --sup

---
+++ lib/tigerbeetlex.ex
@@ -0,0 +1,18 @@
+defmodule Tigerbeetlex do
+  @moduledoc """
+  Documentation for `Tigerbeetlex`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Tigerbeetlex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/tigerbeetlex_test.exs
@@ -0,0 +1,8 @@
+defmodule TigerbeetlexTest do
+  use ExUnit.Case
+  doctest Tigerbeetlex
+
+  test "greets the world" do
+    assert Tigerbeetlex.hello() == :world
+  end
+end

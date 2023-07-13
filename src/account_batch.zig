rbino/tigerbeetlex

commit c17aa29f0c2f20e3fc0e7fa7857d7e5b767c2099

Keep the payload resource until we reach on_completion
Close #12

src/client.zig
@@ -21,6 +21,7 @@ pub const Client = struct {
 const RequestContext = struct {
     caller_pid: beam.pid,
     request_ref_binary: beam.binary,
+    payload_resource: *anyopaque,
 };
 
 pub fn init(env: beam.env, argc: c_int, argv: [*c]const beam.term) callconv(.C) beam.term {
@@ -124,6 +125,13 @@ fn submit(
         return beam.make_error_atom(env, "out_of_memory");
     }
 
+    // We increase the reference count on the payload resource so it doesn't get garbage
+    // collected until we release it
+    e.enif_keep_resource(&payload);
+
+    // We save the pointer in the context so we can release it later
+    ctx.payload_resource = &payload;
+
     packet.operation = @enumToInt(operation);
 
     // TODO: how much does this need to be valid? Should we increment the refcount on the
@@ -179,6 +187,9 @@ fn on_completion(
 ) callconv(.C) void {
     var ctx = @ptrCast(*RequestContext, @alignCast(@alignOf(*RequestContext), packet.user_data.?));
     defer beam.general_purpose_allocator.destroy(ctx);
+
+    // We don't need the payload anymore, let the garbage collector take care of it
+    e.enif_release_resource(ctx.payload_resource);
 
     const env = @intToPtr(*e.ErlNifEnv, context);
     defer e.enif_clear_env(env);

commit 6d5ea5541d70c617832a8bdfc22e39b0ca632279

Completely rework NIF resource handling
Implement our own resource module instead of using the one from Zigler.
Thanks to this, fix bug introduced in c17aa29f where the pointer passed to
enif_resource_release was the wrong one.

src/account_batch.zig
@@ -4,12 +4,12 @@ const batch = @import("batch.zig");
 const beam = @import("beam");
 const beam_extras = @import("beam_extras.zig");
 const e = @import("erl_nif");
-const resource_types = @import("resource_types.zig");
 
 const tb = @import("tigerbeetle");
 const Account = tb.Account;
 const AccountFlags = tb.AccountFlags;
 pub const AccountBatch = batch.Batch(Account);
+pub const AccountBatchResource = batch.BatchResource(Account);
 
 pub fn create(env: beam.env, argc: c_int, argv: [*c]const beam.term) callconv(.C) beam.term {
     if (argc != 1) unreachable;
@@ -52,10 +52,12 @@ fn field_setter_fn(comptime field: std.meta.FieldEnum(Account)) fn (
 
             const args = @ptrCast([*]const beam.term, argv)[0..@intCast(usize, argc)];
 
-            const account_batch = beam_extras.resource_ptr(AccountBatch, env, resource_types.account_batch, args[0]) catch |err|
+            const batch_term = args[0];
+            const account_batch_resource = AccountBatchResource.from_term_handle(env, batch_term) catch |err|
                 switch (err) {
-                error.FetchError => return beam.make_error_atom(env, "invalid_batch"),
+                error.InvalidResourceTerm => return beam.make_error_atom(env, "invalid_batch"),
             };
+            const account_batch = account_batch_resource.ptr();
 
             const idx: u32 = beam.get_u32(env, args[1]) catch
                 return beam.raise_function_clause_error(env);

src/batch.zig
@@ -4,15 +4,11 @@ const Mutex = std.Thread.Mutex;
 const beam = @import("beam");
 const beam_extras = @import("beam_extras.zig");
 const e = @import("erl_nif");
-const resource = beam.resource;
-const resource_types = @import("resource_types.zig");
+const Resource = @import("resource.zig").Resource;
 
 const tb = @import("tigerbeetle");
 const Account = tb.Account;
 const Transfer = tb.Transfer;
-
-pub const AccountBatch = Batch(Account);
-pub const TransferBatch = Batch(Transfer);
 
 pub fn Batch(comptime T: anytype) type {
     return struct {
@@ -28,30 +24,27 @@ pub fn create(comptime T: anytype, env: beam.env, capacity: u32) beam.term {
         error.OutOfMemory => return beam.make_error_atom(env, "out_of_memory"),
     };
 
-    const resource_type = resource_types.from_batch_type(T);
-
     const batch = Batch(T){
         .items = items,
         .len = 0,
     };
-    const batch_resource = resource.create(Batch(T), env, resource_type, batch) catch |err|
+
+    const batch_resource = BatchResource(T).init(batch) catch |err|
         switch (err) {
         error.OutOfMemory => return beam.make_error_atom(env, "out_of_memory"),
     };
+    const term_handle = batch_resource.term_handle(env);
+    batch_resource.release();
 
-    // We immediately release the resource and just let it be managed by the garbage collector
-    // TODO: check all the corner cases to ensure this is the right thing to do
-    resource.release(env, resource_type, batch_resource);
-
-    return beam.make_ok_term(env, batch_resource);
+    return beam.make_ok_term(env, term_handle);
 }
 
 pub fn add_item(comptime T: anytype, env: beam.env, batch_term: beam.term) !beam.term {
-    const resource_type = resource_types.from_batch_type(T);
-    const batch = beam_extras.resource_ptr(Batch(T), env, resource_type, batch_term) catch |err|
+    const batch_resource = BatchResource(T).from_term_handle(env, batch_term) catch |err|
         switch (err) {
-        error.FetchError => return beam.make_error_atom(env, "invalid_batch"),
+        error.InvalidResourceTerm => return beam.make_error_atom(env, "invalid_batch"),
     };
+    const batch = batch_resource.ptr();
 
     {
         if (!batch.mutex.tryLock()) {
@@ -66,4 +59,21 @@ pub fn add_item(comptime T: anytype, env: beam.env, batch_term: beam.term) !beam
 
         return beam.make_ok_term(env, beam.make_u32(env, batch.len));
     }
+}
+
+pub fn BatchResource(comptime T: anytype) type {
+    return Resource(Batch(T), batch_resource_deinit_fn(Batch(T)));
+}
+
+fn batch_resource_deinit_fn(
+    comptime T: anytype,
+) fn (env: beam.env, ptr: ?*anyopaque) callconv(.C) void {
+    return struct {
+        fn deinit_fn(_: beam.env, ptr: ?*anyopaque) callconv(.C) void {
+            if (ptr) |p| {
+                const batch: *T = @ptrCast(*T, @alignCast(@alignOf(*T), p));
+                beam.general_purpose_allocator.free(batch.items);
+            } else unreachable;
+        }
+    }.deinit_fn;
 }

src/beam_extras.zig
@@ -1,32 +1,10 @@
 const std = @import("std");
 const beam = @import("beam");
 const e = @import("erl_nif");
-const resource = beam.resource;
 
 /// Raise a generic exception
 pub fn raise(env: beam.env, reason: []const u8) beam.term {
     return e.enif_raise_exception(env, beam.make_atom(env, reason));
-}
-
-/// Return a pointer to a mutable resource, more ergonomic than doing fetch + update
-pub fn resource_ptr(comptime T: type, environment: beam.env, res_typ: beam.resource_type, res_trm: beam.term) !*T {
-    var obj: ?*anyopaque = undefined;
-
-    if (0 == e.enif_get_resource(environment, res_trm, res_typ, @ptrCast([*c]?*anyopaque, &obj))) {
-        return resource.ResourceError.FetchError;
-    }
-
-    // according to the erlang documentation:
-    // the pointer received in *objp is guaranteed to be valid at least as long as the
-    // resource handle term is valid.
-
-    if (obj == null) {
-        unreachable;
-    }
-
-    var val: *T = @ptrCast(*T, @alignCast(@alignOf(*T), obj));
-
-    return val;
 }
 
 /// Extract a u128 from a binary (little endian) term

src/client.zig
@@ -2,7 +2,6 @@ const std = @import("std");
 
 const beam = @import("beam");
 const e = @import("erl_nif");
-const resource = beam.resource;
 
 const tb = @import("tigerbeetle");
 const tb_client = @import("tigerbeetle/src/clients/c/tb_client.zig");
@@ -11,17 +10,19 @@ const Transfer = tb.Transfer;
 
 const batch = @import("batch.zig");
 const beam_extras = @import("beam_extras.zig");
-const resource_types = @import("resource_types.zig");
 const Batch = batch.Batch;
+const BatchResource = batch.BatchResource;
+const Resource = @import("resource.zig").Resource;
 
 pub const Client = struct {
     zig_client: tb_client.tb_client_t,
 };
+pub const ClientResource = Resource(Client, client_resource_deinit_fn);
 
 const RequestContext = struct {
     caller_pid: beam.pid,
     request_ref_binary: beam.binary,
-    payload_resource: *anyopaque,
+    payload_resource_ptr: *anyopaque,
 };
 
 pub fn init(env: beam.env, argc: c_int, argv: [*c]const beam.term) callconv(.C) beam.term {
@@ -56,7 +57,7 @@ pub fn init(env: beam.env, argc: c_int, argv: [*c]const beam.term) callconv(.C) 
     };
 
     const client = .{ .zig_client = zig_client };
-    const client_resource = resource.create(Client, env, resource_types.client, client) catch |err|
+    const client_resource = ClientResource.init(client) catch |err|
         switch (err) {
         error.OutOfMemory => {
             // Deinit the client
@@ -65,12 +66,10 @@ pub fn init(env: beam.env, argc: c_int, argv: [*c]const beam.term) callconv(.C) 
             return beam.make_error_atom(env, "out_of_memory");
         },
     };
+    const term_handle = client_resource.term_handle(env);
+    client_resource.release();
 
-    // We immediately release the resource and just let it be managed by the garbage collector
-    // TODO: check all the corner cases to ensure this is the right thing to do
-    resource.release(env, resource_types.client, client_resource);
-
-    return beam.make_ok_term(env, client_resource);
+    return beam.make_ok_term(env, term_handle);
 }
 
 fn batch_item_type_for_operation(comptime operation: tb_client.tb_operation_t) type {
@@ -87,16 +86,18 @@ fn submit(
     client_term: beam.term,
     payload_term: beam.term,
 ) beam.term {
-    const client = beam_extras.resource_ptr(Client, env, resource_types.client, client_term) catch |err|
+    const client_resource = ClientResource.from_term_handle(env, client_term) catch |err|
         switch (err) {
-        error.FetchError => return beam.make_error_atom(env, "invalid_client"),
+        error.InvalidResourceTerm => return beam.make_error_atom(env, "invalid_client"),
     };
+    const client = client_resource.ptr_const();
 
     const T = batch_item_type_for_operation(operation);
-    var payload: Batch(T) = resource.fetch(Batch(T), env, resource_types.from_batch_type(T), payload_term) catch |err|
+    const payload_resource = BatchResource(T).from_term_handle(env, payload_term) catch |err|
         switch (err) {
-        error.FetchError => return beam.make_error_atom(env, "invalid_batch"),
+        error.InvalidResourceTerm => return beam.make_error_atom(env, "invalid_batch"),
     };
+    const payload = payload_resource.ptr_const();
 
     var out_packet: ?*tb_client.tb_packet_t = undefined;
     const status = tb_client.acquire_packet(client.zig_client, &out_packet);
@@ -127,10 +128,10 @@ fn submit(
 
     // We increase the reference count on the payload resource so it doesn't get garbage
     // collected until we release it
-    e.enif_keep_resource(&payload);
+    payload_resource.keep();
 
-    // We save the pointer in the context so we can release it later
-    ctx.payload_resource = &payload;
+    // We save the raw pointer in the context so we can release it later with enif_release_resource
+    ctx.payload_resource_ptr = payload_resource.raw_ptr;
 
     packet.operation = @enumToInt(operation);
 
@@ -189,7 +190,8 @@ fn on_completion(
     defer beam.general_purpose_allocator.destroy(ctx);
 
     // We don't need the payload anymore, let the garbage collector take care of it
-    e.enif_release_resource(ctx.payload_resource);
+    // This is a raw resource pointer so we directly call enif_release_resource
+    e.enif_release_resource(ctx.payload_resource_ptr);
 
     const env = @intToPtr(*e.ErlNifEnv, context);
     defer e.enif_clear_env(env);
@@ -228,4 +230,14 @@ fn on_completion(
 
     // We're done with the packet, put it back in the pool
     tb_client.release_packet(client, packet);
+}
+
+fn client_resource_deinit_fn(_: beam.env, ptr: ?*anyopaque) callconv(.C) void {
+    if (ptr) |p| {
+        const cl: *Client = @ptrCast(*Client, @alignCast(@alignOf(*Client), p));
+        // TODO: this can now potentially block for a long time since it waits
+        // for all the requests to be drained, investigate what it is blocking
+        // and if this needs to be done in a separate thread
+        tb_client.deinit(cl.zig_client);
+    } else unreachable;
 }

src/id_batch.zig
@@ -5,9 +5,9 @@ const batch = @import("batch.zig");
 const beam = @import("beam");
 const beam_extras = @import("beam_extras.zig");
 const e = @import("erl_nif");
-const resource_types = @import("resource_types.zig");
 
 pub const IdBatch = batch.Batch(u128);
+pub const IdBatchResource = batch.BatchResource(u128);
 
 pub fn create(env: beam.env, argc: c_int, argv: [*c]const beam.term) callconv(.C) beam.term {
     if (argc != 1) unreachable;
@@ -29,11 +29,12 @@ pub fn add_id(env: beam.env, argc: c_int, argv: [*c]const beam.term) callconv(.C
     const id = beam_extras.get_u128(env, args[1]) catch
         return beam.raise_function_clause_error(env);
 
-    const resource_type = resource_types.id_batch;
-    const id_batch = beam_extras.resource_ptr(IdBatch, env, resource_type, args[0]) catch |err|
+    const batch_term = args[0];
+    const id_batch_resource = IdBatchResource.from_term_handle(env, batch_term) catch |err|
         switch (err) {
-        error.FetchError => return beam.make_error_atom(env, "invalid_batch"),
+        error.InvalidResourceTerm => return beam.make_error_atom(env, "invalid_batch"),
     };
+    const id_batch = id_batch_resource.ptr();
 
     {
         if (!id_batch.mutex.tryLock()) {
@@ -56,11 +57,12 @@ pub fn set_id(env: beam.env, argc: c_int, argv: [*c]const beam.term) callconv(.C
 
     const args = @ptrCast([*]const beam.term, argv)[0..@intCast(usize, argc)];
 
-    const resource_type = resource_types.id_batch;
-    const id_batch = beam_extras.resource_ptr(IdBatch, env, resource_type, args[0]) catch |err|
+    const batch_term = args[0];
+    const id_batch_resource = IdBatchResource.from_term_handle(env, batch_term) catch |err|
         switch (err) {
-        error.FetchError => return beam.make_error_atom(env, "invalid_batch"),
+        error.InvalidResourceTerm => return beam.make_error_atom(env, "invalid_batch"),
     };
+    const id_batch = id_batch_resource.ptr();
 
     const idx: u32 = beam.get_u32(env, args[1]) catch
         return beam.raise_function_clause_error(env);

src/resource.zig
@@ -1,0 +1,122 @@
+const std = @import("std");
+
+const assert = std.debug.assert;
+const beam = @import("beam");
+const e = @import("erl_nif");
+
+pub const Error = error{
+    InvalidResourceTerm,
+    OutOfMemory,
+};
+
+const DeinitFn = fn (beam.env, ?*anyopaque) callconv(.C) void;
+
+pub fn Resource(comptime T: anytype, comptime deinit_fn: ?DeinitFn) type {
+    return struct {
+        const Self = @This();
+
+        const Type = struct {
+            beam_type: beam.resource_type,
+
+            pub fn open(env: beam.env) Type {
+                const beam_type = e.enif_open_resource_type(
+                    env,
+                    null,
+                    "TigerBeetlex." ++ @typeName(T),
+                    deinit_fn,
+                    e.ERL_NIF_RT_CREATE | e.ERL_NIF_RT_TAKEOVER,
+                    null,
+                );
+
+                assert(beam_type != null);
+
+                return .{ .beam_type = beam_type };
+            }
+        };
+
+        var resource_type: ?Type = null;
+
+        raw_ptr: *anyopaque,
+
+        /// Initializes the type of the resource. This must be called exactly once
+        /// in the load or upgrade callback of the NIF.
+        pub fn create_type(env: beam.env) void {
+            // TODO: is this required or are we allowed to re-open a type?
+            assert(resource_type == null);
+
+            resource_type = Type.open(env);
+        }
+
+        /// Allocates the memory of the resource
+        pub fn alloc() !Self {
+            var raw_ptr: ?*anyopaque = e.enif_alloc_resource(res_type(), @sizeOf(T));
+
+            if (raw_ptr) |p| {
+                return Self{ .raw_ptr = p };
+            } else {
+                return error.OutOfMemory;
+            }
+        }
+
+        /// Allocates the memory of the resource and initializes it with a value
+        pub fn init(val: T) !Self {
+            const ret = try Self.alloc();
+
+            const value_ptr = ret.ptr();
+            value_ptr.* = val;
+
+            return ret;
+        }
+
+        /// Recreates the resource from the term handle obtained with `term_handle`
+        pub fn from_term_handle(env: beam.env, term: beam.term) !Self {
+            var raw_ptr: ?*anyopaque = undefined;
+
+            if (0 == e.enif_get_resource(env, term, res_type(), &raw_ptr)) {
+                return error.InvalidResourceTerm;
+            }
+
+            return Self{ .raw_ptr = raw_ptr.? };
+        }
+
+        /// Obtains a term handle to the resource
+        pub fn term_handle(self: Self, env: beam.env) beam.term {
+            return e.enif_make_resource(env, self.raw_ptr);
+        }
+
+        /// Decreases the refcount of a resource
+        pub fn release(self: Self) void {
+            e.enif_release_resource(self.raw_ptr);
+        }
+
+        /// Increases the refcount of a resource
+        pub fn keep(self: Self) void {
+            e.enif_keep_resource(self.raw_ptr);
+        }
+
+        /// Returns the allocation size of the resource
+        pub fn size(self: Self) usize {
+            return e.enif_sizeof_resource(self.raw_ptr);
+        }
+
+        /// Returns a copy of the resource value
+        pub fn value(self: Self) T {
+            const value_ptr = self.ptr_const();
+            return value_ptr.*;
+        }
+
+        /// Returns a const pointer to the resource value
+        pub fn ptr_const(self: Self) *const T {
+            return @ptrCast(*const T, @alignCast(@alignOf(*const T), self.raw_ptr));
+        }
+
+        /// Returns a pointer to the resource value
+        pub fn ptr(self: Self) *T {
+            return @ptrCast(*T, @alignCast(@alignOf(*T), self.raw_ptr));
+        }
+
+        fn res_type() beam.resource_type {
+            return resource_type.?.beam_type;
+        }
+    };
+}

src/resource_types.zig
@@ -1,49 +1,0 @@
-const beam = @import("beam");
-
-const tb = @import("tigerbeetle");
-const tb_client = @import("tigerbeetle/src/clients/c/tb_client.zig");
-const Account = tb.Account;
-const Client = @import("client.zig").Client;
-const Transfer = tb.Transfer;
-
-// The resource type for the client
-pub var client: beam.resource_type = undefined;
-
-// The resource type for the account batch
-pub var account_batch: beam.resource_type = undefined;
-
-// The resource type for an ID batch
-pub var id_batch: beam.resource_type = undefined;
-
-// The resource type for the transfer batch
-pub var transfer_batch: beam.resource_type = undefined;
-
-pub fn from_batch_type(comptime T: anytype) beam.resource_type {
-    return switch (T) {
-        Account => account_batch,
-        u128 => id_batch,
-        Transfer => transfer_batch,
-        else => unreachable,
-    };
-}
-
-pub fn client_deinit_fn(_: beam.env, ptr: ?*anyopaque) callconv(.C) void {
-    if (ptr) |p| {
-        const cl: *Client = @ptrCast(*Client, @alignCast(@alignOf(*Client), p));
-        // TODO: this can now potentially block for a long time since it waits
-        // for all the requests to be drained, investigate what it is blocking
-        // and if this needs to be done in a separate thread
-        tb_client.deinit(cl.zig_client);
-    } else unreachable;
-}
-
-pub fn batch_deinit_fn(comptime T: anytype) fn (env: beam.env, ptr: ?*anyopaque) callconv(.C) void {
-    return struct {
-        fn deinit_fn(_: beam.env, ptr: ?*anyopaque) callconv(.C) void {
-            if (ptr) |p| {
-                const b: *T = @ptrCast(*T, @alignCast(@alignOf(*T), p));
-                beam.general_purpose_allocator.free(b.items);
-            } else unreachable;
-        }
-    }.deinit_fn;
-}

src/tigerbeetlex.zig
@@ -5,12 +5,17 @@ const e = @import("erl_nif");
 const account_batch = @import("account_batch.zig");
 const client = @import("client.zig");
 const id_batch = @import("id_batch.zig");
-const resource_types = @import("resource_types.zig");
+const resource = @import("resource.zig");
 const transfer_batch = @import("transfer_batch.zig");
 const AccountBatch = account_batch.AccountBatch;
 const IdBatch = id_batch.IdBatch;
 const TransferBatch = transfer_batch.TransferBatch;
 const Client = client.Client;
+
+const ClientResource = client.ClientResource;
+const AccountBatchResource = account_batch.AccountBatchResource;
+const IdBatchResource = id_batch.IdBatchResource;
+const TransferBatchResource = transfer_batch.TransferBatchResource;
 
 const vsr = @import("tigerbeetle/src/vsr.zig");
 pub const vsr_options = .{
@@ -213,37 +218,9 @@ export fn nif_init() *const e.ErlNifEntry {
 }
 
 export fn nif_load(env: beam.env, _: [*c]?*anyopaque, _: beam.term) c_int {
-    resource_types.client = e.enif_open_resource_type(
-        env,
-        null,
-        "tigerbeetlex_client",
-        resource_types.client_deinit_fn,
-        e.ERL_NIF_RT_CREATE | e.ERL_NIF_RT_TAKEOVER,
-        null,
-    );
-    resource_types.account_batch = e.enif_open_resource_type(
-        env,
-        null,
-        "tigerbeetlex_account_batch",
-        resource_types.batch_deinit_fn(AccountBatch),
-        e.ERL_NIF_RT_CREATE | e.ERL_NIF_RT_TAKEOVER,
-        null,
-    );
-    resource_types.id_batch = e.enif_open_resource_type(
-        env,
-        null,
-        "tigerbeetlex_id_batch",
-        resource_types.batch_deinit_fn(IdBatch),
-        e.ERL_NIF_RT_CREATE | e.ERL_NIF_RT_TAKEOVER,
-        null,
-    );
-    resource_types.transfer_batch = e.enif_open_resource_type(
-        env,
-        null,
-        "tigerbeetlex_transfer_batch",
-        resource_types.batch_deinit_fn(TransferBatch),
-        e.ERL_NIF_RT_CREATE | e.ERL_NIF_RT_TAKEOVER,
-        null,
-    );
+    ClientResource.create_type(env);
+    AccountBatchResource.create_type(env);
+    IdBatchResource.create_type(env);
+    TransferBatchResource.create_type(env);
     return 0;
 }

src/transfer_batch.zig
@@ -4,12 +4,12 @@ const batch = @import("batch.zig");
 const beam = @import("beam");
 const beam_extras = @import("beam_extras.zig");
 const e = @import("erl_nif");
-const resource_types = @import("resource_types.zig");
 
 const tb = @import("tigerbeetle");
 const Transfer = tb.Transfer;
 const TransferFlags = tb.TransferFlags;
 pub const TransferBatch = batch.Batch(Transfer);
+pub const TransferBatchResource = batch.BatchResource(Transfer);
 
 pub fn create(env: beam.env, argc: c_int, argv: [*c]const beam.term) callconv(.C) beam.term {
     if (argc != 1) unreachable;
@@ -57,10 +57,12 @@ fn field_setter_fn(comptime field: std.meta.FieldEnum(Transfer)) fn (
 
             const args = @ptrCast([*]const beam.term, argv)[0..@intCast(usize, argc)];
 
-            const transfer_batch = beam_extras.resource_ptr(TransferBatch, env, resource_types.transfer_batch, args[0]) catch |err|
+            const batch_term = args[0];
+            const transfer_batch_resource = TransferBatchResource.from_term_handle(env, batch_term) catch |err|
                 switch (err) {
-                error.FetchError => return beam.make_error_atom(env, "invalid_batch"),
+                error.InvalidResourceTerm => return beam.make_error_atom(env, "invalid_batch"),
             };
+            const transfer_batch = transfer_batch_resource.ptr();
 
             const idx: u32 = beam.get_u32(env, args[1]) catch
                 return beam.raise_function_clause_error(env);
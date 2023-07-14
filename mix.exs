4f53363469aac911949d4bb24d04e34b12a82914
Riccardo Binetti <rbino@gmx.com>
2023-07-14 23:39:46 +0200

    Switch back zigler to upstream repo
    The PR we needed were merged. Also tidy up mix.exs.

--- mix.exs
+++ mix.exs
@@ -29,12 +29,10 @@ defmodule TigerBeetlex.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:nimble_options, "~> 1.0", tuntime: false},
-      {:build_dot_zig, "~> 0.2.0", runtime: false},
-      {:zigler, github: "rbino/zigler", branch: "free-large-alloc", runtime: false},
-      # Needed to make Zigler work
-      {:ex_doc, "== 0.29.0", runtime: false, override: true},
       {:typed_struct, "~> 0.3.0"},
+      {:nimble_options, "~> 1.0"},
+      {:build_dot_zig, "~> 0.2.0", runtime: false},
+      {:zigler, github: "ityonemo/zigler", runtime: false},
       {:elixir_uuid, "~> 1.2", only: :test},
       {:dialyxir, "~> 1.3", only: [:dev, :test], runtime: false}
     ]

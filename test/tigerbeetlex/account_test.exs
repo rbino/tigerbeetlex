387460c50442f141cf02836fd923bac95e5f45cc
Riccardo Binetti <rbino@gmx.com>
2023-08-16 22:34:44 +0200

    account,transfer: add to_batch_item/1
    Serialize an Account/Transfer to a binary on the Elixir side, ignoring
    server-controlled fields.
    Also add relevant tests.

---
+++ test/tigerbeetlex/account_test.exs
@@ -0,0 +1,50 @@
+defmodule Tigerbeetlex.AccountTest do
+  use ExUnit.Case
+
+  alias TigerBeetlex.Account
+
+  test "from_binary and to_batch_item round trip" do
+    account = %Account{
+      id: <<1234::128>>,
+      user_data: <<5678::128>>,
+      ledger: 42,
+      code: 99,
+      flags: %Account.Flags{credits_must_not_exceed_debits: true}
+    }
+
+    assert %Account{
+             id: <<1234::128>>,
+             user_data: <<5678::128>>,
+             ledger: 42,
+             code: 99,
+             flags: %Account.Flags{credits_must_not_exceed_debits: true}
+           } =
+             account
+             |> Account.to_batch_item()
+             |> Account.from_binary()
+  end
+
+  test "to_batch_item/1 ignores server-controlled fields" do
+    account = %Account{
+      id: <<1234::128>>,
+      ledger: 42,
+      code: 99,
+      debits_pending: 1,
+      debits_posted: 2,
+      credits_pending: 3,
+      credits_posted: 4,
+      timestamp: 5
+    }
+
+    assert %Account{
+             debits_pending: 0,
+             debits_posted: 0,
+             credits_pending: 0,
+             credits_posted: 0,
+             timestamp: 0
+           } =
+             account
+             |> Account.to_batch_item()
+             |> Account.from_binary()
+  end
+end

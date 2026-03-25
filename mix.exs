f5c33edf6a94189068c5e6607e20aeb06ad1b43f
Riccardo Binetti <rbino@gmx.com>
2026-03-25 21:06:34 +0100

    chore: prepare release 0.16.78

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule TigerBeetlex.MixProject do
   use Mix.Project
 
-  @version "0.16.77"
+  @version "0.16.78"
 
   @repo_url "https://github.com/rbino/tigerbeetlex"
 
--- src/config.zig
+++ src/config.zig
@@ -1,5 +1,5 @@
 pub const vsr_options = .{
-    .release = "0.16.77",
+    .release = "0.16.78",
     .release_client_min = "0.16.4",
     .git_commit = null,
     .hash_log_mode = .none,
